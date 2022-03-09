8bb5f241c71ef1d8c7c46238a4009fc357fde89a
scottly426 <lyscott123@gmail.com>
2022-03-08 21:33:15 -0800

    build

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "https://stark-ridge-32063.herokuapp.com/";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
